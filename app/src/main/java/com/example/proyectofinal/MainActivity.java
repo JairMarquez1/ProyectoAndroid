b04d4884b1518ccacd32db658c5dcf22d5fe12c0
JairMarquez1 <a19310186@ceti.mx>
2021-05-03 22:19:24 -0500

    Version 0.1.1 Base de datos

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyectofinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,10 +8,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     public EditText etEmail, etPass;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
         Intent intent1 = new Intent(this, Index.class);
         intent1.putExtra("email",email);
         intent1.putExtra("pass",pass);
+
+        //Prueba de base de datos--------------------------
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        user.put("pass", pass);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this,"Bien",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"Mal",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        //--------Aqui termina la prueba----------------------------
+
 
         startActivity(intent1);
 
